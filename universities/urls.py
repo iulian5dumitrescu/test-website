39bfbd85d740a5819787d92591ce73bdec49ebe1
Parrot278 <100129897+Parrot278@users.noreply.github.com>
2025-03-02 20:29:10 +0000

    Added stuff

--- universities/urls.py
+++ universities/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("catalogfacultati/", views.catalog_facultati, name = "catalogfacultati")
 ]
--- universities/views.py
+++ universities/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Facultate
 
 # Create your views here.
 
 def home(request):
     context = {"message": "Cam greu sa programezi :("}
     return render(request, "universities/meniu.html", context)
+
+def catalog_facultati(request):
+    items = Facultate.objects.all()
+    return render(request, "universities/catalogfacultati.html", {"facultati": items} )
