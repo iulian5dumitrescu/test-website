e0a63911eda70eac1b4eaad8e7ccbbb05fa2984c
Parrot278 <100129897+Parrot278@users.noreply.github.com>
2025-03-02 14:52:41 +0000

    Created index.html

--- universities/views.py
+++ universities/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Salut, prima aplicatie")
+    context = {"message": "Cam greu sa programezi :("}
+    return render(request, "universities/index.html", context)
