9bc9e244e8cb3969aa267446157a6d567952c6b6
Parrot278 <100129897+Parrot278@users.noreply.github.com>
2025-03-03 19:49:40 +0000

    Link multiple HTML templates with URLs

--- universities/urls.py
+++ universities/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ecran_principal, name="ecran_principal"),
+    path("", views.ecran_principal, name="ecranprincipal"),
     path("facultatiromania/", views.facultati_romania, name = "facultatiromania"),
     path("catalogfacultati/", views.catalog_facultati, name = "catalogfacultati"),
+    path("chestionar/", views.chestionar, name = "chestionar"),
     path("bibliografie/", views.bibliografie, name = "bibliografie"),
+    path("cvmeniu/", views.cv_meniu, name = "cvmeniu")
 ]
--- universities/views.py
+++ universities/views.py
@@ -14,5 +14,11 @@ def catalog_facultati(request):
     items = Facultate.objects.all()
     return render(request, "universities/catalogfacultati.html", {"facultati": items} )
 
+def cv_meniu(request):
+    return render(request, "universities/cv.de.succes.html")
+
+def chestionar(request):
+    return render(request, "universities/chestionar.html")
+
 def bibliografie(request):
     return render(request, "universities/bibliografie.html")
